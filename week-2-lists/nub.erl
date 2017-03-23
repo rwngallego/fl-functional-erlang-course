179f97425d17730a8ed9e97a9ff2a966da44ec02
Rowinson Gallego <rgallego@alertlogic.com>
2017-03-22 23:27:26 -0500

    nub added

---
+++ week-2-lists/nub.erl
@@ -0,0 +1,30 @@
+-module(nub).
+
+-export([nub/1, contains/2, test/0]).
+
+% remove all the duplicate elements from a list
+-spec nub([T])->[T].
+
+nub(L)->
+    Result = nub(L, []),
+    lists:reverse(Result).
+nub([], R) -> R;
+nub([H|T], R)->
+    case contains(R, H) of
+        {ok, Value} when Value == false ->
+            nub(T, [H|R]);
+        {ok, Value} when Value == true ->
+            nub(T, R)
+    end.
+
+contains([], _E) ->
+    {ok, false};
+contains([H|T], E) ->
+    if
+        H==E -> {ok, true};
+        true -> contains(T, E)
+    end.
+
+test()->
+    [2,4,1,3] = nub([2,4,1,3,3,1]),
+    ok.
